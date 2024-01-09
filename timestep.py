5761870d827192c4a4bfb5f73684790ca2478cdb
JianguoHe <inkybamboo@gmail.com>
2024-01-09 15:12:39 +0800

    timestep合并

--- timestep.py
+++ timestep.py
@@ -12,45 +12,45 @@ def timestep(self):   #self.type, self.age, self.tm, self.tn, self.tscls, dt):
     # pts2 = 0.01      CHeB, GB, AGB, HeGB
     # pts3 = 0.02      HG, HeMS
     if self.type <= 1:
-        self.dt = pts1 * self.tm
+        dt = pts1 * self.tm
         dtr = self.tm - self.age
     elif self.type == 2:
-        self.dt = pts3 * (self.tscls[1] - self.tm)      # 【更改】把这里的 pts1 改成 pts3, 缩短 HG 的演化步长
+        dt = pts3 * (self.tscls[1] - self.tm)      # 【更改】把这里的 pts1 改成 pts3, 缩短 HG 的演化步长
         dtr = self.tscls[1] - self.age
     elif self.type == 3:
         if self.age < self.tscls[6]:
-            self.dt = pts2 * (self.tscls[4] - self.age)
+            dt = pts2 * (self.tscls[4] - self.age)
         else:
-            self.dt = pts2 * (self.tscls[5] - self.age)
+            dt = pts2 * (self.tscls[5] - self.age)
         dtr = min(self.tscls[2], self.tn) - self.age
     elif self.type == 4:
-        self.dt = pts2 * self.tscls[3]
+        dt = pts2 * self.tscls[3]
         dtr = min(self.tn, self.tscls[2] + self.tscls[3]) - self.age
     elif self.type == 5:
         if self.age < self.tscls[9]:
-            self.dt = pts3 * (self.tscls[7] - self.age)
+            dt = pts3 * (self.tscls[7] - self.age)
         else:
-            self.dt = pts3 * (self.tscls[8] - self.age)
+            dt = pts3 * (self.tscls[8] - self.age)
         dtr = min(self.tn, self.tscls[13]) - self.age
     elif self.type == 6:
         if self.age < self.tscls[12]:
-            self.dt = pts3 * (self.tscls[10] - self.age)
+            dt = pts3 * (self.tscls[10] - self.age)
         else:
-            self.dt = pts3 * (self.tscls[11] - self.age)
-        self.dt = min(self.dt, 0.005)
+            dt = pts3 * (self.tscls[11] - self.age)
+        dt = min(dt, 0.005)
         dtr = self.tn - self.age
     elif self.type == 7:
-        self.dt = pts1 * self.tm
+        dt = pts1 * self.tm
         dtr = self.tm - self.age
     elif self.type == 8 or self.type == 9:
         if self.age < self.tscls[6]:
-            self.dt = pts2 * (self.tscls[4] - self.age)
+            dt = pts2 * (self.tscls[4] - self.age)
         else:
-            self.dt = pts2 * (self.tscls[5] - self.age)
+            dt = pts2 * (self.tscls[5] - self.age)
         dtr = self.tn - self.age
     else:
-        self.dt = max(0.1, self.dt * 10.0)
-        self.dt = min(self.dt, 5.0e2)
-        dtr = self.dtdt
+        dt = min(max(0.1, self.dt * 10.0), 500)
+        dtr = dt
 
-    return dtr
+    self.dt = min(dt, dtr)
+
